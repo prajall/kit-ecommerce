eee25f72bda5d6963e9caab469ff6144dbdc84b9
prajall <prajalmhrzn@gmail.com>
2024-05-17 10:03:57 +0545

    change in get product

--- src/app/api/getproduct/route.ts
+++ src/app/api/getproduct/route.ts
@@ -1,24 +1,24 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async( req:NextRequest)=>{
+export const GET = async (req: NextRequest) => {
   try {
-    console.log("sadasd")
+    console.log("sadasd");
     //  console.log(data)
-    return NextResponse.json({message: "success"})
+    return NextResponse.json({ message: "success" });
   } catch (error) {
-    console.log("error routets: ",error)
-    return NextResponse.json({"error routets":"error"})
+    console.log("error routets: ", error);
+    return NextResponse.json({ "error routets": "error" });
   }
-}
-export const POST = async( req:NextRequest)=>{
+};
+export const POST = async (req: NextRequest) => {
   try {
-    
-    const data =  await req.json()
-    console.log(data)
+    const data = await req.json();
+    console.log(data);
     //  console.log(data)
-    return NextResponse.json({message: "success"})
+    return NextResponse.json({ message: "success" });
   } catch (error) {
-    console.log("error routets: ",error)
-    return NextResponse.json({"error routets":"error"})
+    console.log("error routets: ", error);
+    return NextResponse.json({ "error routets": "error" });
   }
-}
+};
+// this is a minor changes
