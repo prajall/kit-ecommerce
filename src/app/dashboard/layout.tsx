3d01a419361cd881b079f4814dac4db26ae4273c
prajall <prajalmhrzn@gmail.com>
2024-05-31 21:26:44 +0545

    no redirect

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,30 +1,20 @@
-"use client";
 import { auth } from "@/auth";
-import { CommandSeparator } from "@/components/ui/command";
-import { DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
-import { Separator } from "@radix-ui/react-dropdown-menu";
 import { Poppins } from "next/font/google";
 import { redirect } from "next/navigation";
 import React from "react";
-import { IconType } from "react-icons/lib";
 
-import Link from "next/link";
 import DashboardSidebar from "./(components)/DashboardSidebar";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 
 const font = Poppins({ weight: "400", subsets: ["latin"] });
 
 const AdminLayout = async ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
-  const { data } = useSession();
-  // const user = await auth();
-  console.log(data);
+  const user = await auth();
+  console.log(user);
 
-  if (!data || data.user?.email != "prajalmhrzn@gmail.com") {
-    router.push("/");
-  }
+  // if (!user || user.user?.email != "prajalmhrzn@gmail.com") {
+  //   redirect("/");
+  // }
 
   return (
     <>
